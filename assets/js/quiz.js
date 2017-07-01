b42cc27f3f044fa9c670fd5db94734f29e01973e
Dave <drregg6@gmail.com>
2017-06-30 22:35:27 -0400

    quiz questions complete, rendering functions complete

---
+++ assets/js/quiz.js
@@ -0,0 +1,124 @@
+var questions;
+
+questions = [
+    {
+        number: 0,
+        question: 'Who lived at 25 Cromwell Street?',
+        choices: ['Jack the Ripper', 'Ian Bradley and Myra Hindley', 'Fred and Rose West', 'The Yorkshire Ripper'],
+        answer: 2
+    },
+    {
+        number: 1,
+        question: 'How did Ted Bundy lure his victims in?',
+        choices: ['Pretending to be an officer', 'Pretending to be injured', 'Asking them out on a date', 'Knocking them out'],
+        answer: 1
+    },
+    {
+        number: 2,
+        question: 'What year was the term "serial killer" coined in America?',
+        choices: ['1912', '1996', '1960', '1976'],
+        answer: 2
+    },
+    {
+        number: 3,
+        question: 'What cult inspired Timothy McReign, the Oklahoma City Bomber, to do what he did?',
+        choices: ['The Branen Dividians', 'Children of God', 'The Manson Family', 'Aum Shinrikyo'],
+        answer: 0
+    },
+    {
+        number: 4,
+        question: 'How many people did Ed Gein kill?',
+        choices: ['10', '25', '2', '6'],
+        answer: 2
+    },
+    {
+        number: 5,
+        question: 'How many was Ed Gein convicted for?',
+        choices: ['0', '1', '10', '16'],
+        answer: 1
+    },
+    {
+        number: 6,
+        question: 'What was Brenda Spencer\'s reason for her killing spree at Cleveland Elementary School?',
+        choices: ['She didn\'t like Mondays', 'She was being bullied', 'She wanted to see what killing was like', 'She hated kids'],
+        answer: 0
+    },
+    {
+        number: 7,
+        question: 'What does Dennis Rader\'s nickname, "BTK", stand for?',
+        choices: ['Bind, Torture, Kill', 'Blindfold, Tickle, Kill', 'Bind, Tickle, Kill', 'Blindfold, Torture, Kill'],
+        answer: 0
+    },
+    {
+        number: 8,
+        question: 'What was Sacramento serial killer Richard Chase\'s nickname?',
+        choices: ['The Night Stalker', 'The Vampire of Sacramento', 'The East Area Rapist', 'The Zodiac Killer'],
+        answer: 1
+    },
+    {
+        number: 9,
+        question: 'Which woman was thought to be the first serial killer?',
+        choices: ['Aileen Wuornos', 'Belle Gunness', 'Jane Toppan', 'Lavinia Fisher'],
+        answer: 3
+    },
+    {
+        number: 10,
+        question: 'What book was the torn page from that was found with the Somerton Man?',
+        choices: ['The Rudaiyat of Omar Khayyan', 'The Bible', 'The Great Gatsby', 'The Quaran'],
+        answer: 0
+    },
+    {
+        number: 11,
+        question: 'How many years was Jaycee LeeDugard held against her own will?',
+        choices: ['1 year', '8 years', '25 years', '3 years'],
+        answer: 1
+    },
+    {
+        number: 12,
+        question: 'What disorder did the Eriksson Twins suffer from?',
+        choices: ['Bipolar', 'Schizophrenia', 'Depression', 'Folie a deux'],
+        answer: 3
+    },
+    {
+        number: 13,
+        question: 'What country was the infamous "Who put Bella in the wych-elm" body found?',
+        choices: ['England', 'Ireland', 'Scotland', 'Poland'],
+        answer: 1
+    },
+    {
+        number: 14,
+        question: 'What was John Wayne Gacy known as?',
+        choices: ['The Killer Clown', 'The Midwest Killer', 'The Hillside Strangler', 'The Candyman'],
+        answer: 0
+    },
+    {
+        number: 15,
+        question: 'Who was Leonard Lake\'s partner in crime?',
+        choices: ['Dean Corll', 'Charles Ng', 'David Burkowitz', 'Ottis Toole'],
+        answer: 1
+    },
+    {
+        number: 16,
+        question: 'What country did Andrei Chikatilo hail from?',
+        choices: ['England', 'Japan', 'America', 'Russia'],
+        answer: 3
+    },
+    {
+        number: 17,
+        question: 'What state was The Green River Killer active in?',
+        choices: ['Ohio', 'New York', 'California', 'Washington'],
+        answer: 3
+    },
+    {
+        number: 18,
+        question: 'Who was known to be Jack the Ripper\'s last victim?',
+        choices: ['Martha Tabram', 'Catherine Eddowes', 'Mary Ann Nichols', 'Mary Kelly'],
+        answer: 3
+    },
+    {
+        number: 19,
+        question: 'What was the movie The Zodiac Killer referred to as the best satirical comedy?',
+        choices: ['The Exorcist', 'Halloween', 'Jaws', 'Amityville Horror'],
+        answer: 0
+    }
+];
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,37 +1,30 @@
-var choiceOne = document.getElementById('one');
-var choiceTwo = document.getElementById('two');
-var choiceThree = document.getElementById('three');
-var choiceFour = document.getElementById('four');
+var count = 0;
+
+var choices = document.querySelectorAll('.choices');
 var question = document.getElementsByTagName('h2')[0];
 var nextButton = document.getElementsByClassName('next')[0];
 var prevButton = document.getElementsByClassName('prev')[0];
 
-choiceOne.innerText = 'Hello world!';
-choiceTwo.innerText = 'Dave Regg';
-choiceThree.innerText = 'Yo momma';
-choiceFour.innerText = 'Donald Duck';
-question.innerText = 'Who is the President of the United States?';
-
 window.onload = function() {
-    question.innerText = 'What is Dave Regg\'s favorite color?';
-    choiceOne.innerText = 'Red!';
-    choiceTwo.innerText = 'Orange!';
-    choiceThree.innerText = 'Yellow!';
-    choiceFour.innerText = 'Green!';
+    renderQuestion();
 }
 
 nextButton.addEventListener('click', function(ev) {
-    question.innerText = 'What time is it?';
-    choiceOne.innerText = 'Adventure Time!';
-    choiceTwo.innerText = 'Time to code!';
-    choiceThree.innerText = 'Peanut jelly time!';
-    choiceFour.innerText = 'All I want for Christmas is my two front teeth.';
+    count++;
+    renderQuestion();
 });
 
 prevButton.addEventListener('click', function(ev) {
-    question.innerText = 'What is Dave Regg\'s favorite color?';
-    choiceOne.innerText = 'Red!';
-    choiceTwo.innerText = 'Orange!';
-    choiceThree.innerText = 'Yellow!';
-    choiceFour.innerText = 'Green!';
+    count--;
+    renderQuestion();
 });
+
+
+
+function renderQuestion() {
+    question.innerText = questions[count].question;
+    
+    choices.forEach(function(choice, i) {
+        choice.innerText = questions[count].choices[i];
+    });
+}
